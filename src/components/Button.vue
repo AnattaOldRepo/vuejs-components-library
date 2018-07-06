82f412d02d64d3ae0b9db3143bce7bc8b3d7c5f2
John Potess <jppotess@gmail.com>
2018-07-06 14:32:28 -0500

    used components in progress

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -6,15 +6,17 @@
         :icon="icon"
     >
         <div class="c-btn__inner">
-            <Icon v-if="icon && iconLeft"
+            <Icon v-if="icon && iconLeft && !arrow"
                 class="c-btn__icon"
                 :icon="icon"
             />
             <span class="c-btn__text">{{ text }}</span>
-            <Icon v-if="icon && !iconLeft"
+            <Icon v-if="icon && !iconLeft && !arrow"
                 class="c-btn__icon"
                 :icon="icon"
             />
+            <svg v-if="arrow"
+                class="c-btn__arrow" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512.008 512.008" style="enable-background:new 0 0 512.008 512.008;" xml:space="preserve"><g><g><path d="M507.904,245.317L344.608,63.909c-2.848-3.168-6.848-5.056-11.072-5.28c-4.352-0.288-8.384,1.248-11.552,4.096l-47.552,42.784c-6.56,5.92-7.072,16.032-1.184,22.592l71.904,79.904H16c-8.832,0-16,7.168-16,16v64c0,8.832,7.168,16,16,16h329.152l-71.904,79.872c-5.92,6.592-5.376,16.672,1.184,22.624l47.552,42.816c3.072,2.752,6.912,4.096,10.72,4.096c4.384,0,8.704-1.792,11.904-5.28l163.296-181.408C513.376,260.645,513.376,251.397,507.904,245.317z"/></g></g></svg>
         </div>
     </a>
 </template>
@@ -43,6 +45,13 @@ export default {
 			type: [String, Boolean],
 			default: false
         },
+        /**
+         * Specifically for Arrow buttons
+         */
+		arrow: {
+			type: Boolean,
+			default: false
+        },        
         /**
          * Icon name, if used
          */
@@ -88,11 +97,13 @@ export default {
 
                 'c-btn--secondary': this.alt === 'secondary',
                 'c-btn--accent': this.alt === 'accent',
+                'c-btn--ghost': this.alt === 'ghost',
 
                 'c-btn--iconLeft': this.iconLeft === true,
                 'c-btn--iconRight': this.iconLeft === false,
                 'c-btn--hasIcon': this.icon,
-                'c-btn--isDisabled': this.disabled === true
+                'c-btn--isDisabled': this.disabled === true,
+                'c-btn--arrow': this.arrow === true
             }
         },
     }
@@ -105,29 +116,28 @@ export default {
     <div class="buttons">
         <Button text="Button" size="small" />
         <Button text="Button" size="small" alt="secondary"/>
-        <Button text="Button" size="small" alt="accent"/>
-        <Button text="Button" size="small" alt="accent" disabled/>
-        <Button text="Button" size="small" icon="play"/>
+        <Button text="Button" size="small" alt="ghost"/>
+        <Button text="Button" size="small" disabled/>
         <Button text="Button" size="small" icon="play" iconLeft/>
-
+        <Button text="Button" size="small" arrow/>
     </div>
 
     <div class="buttons">
         <Button text="Button" size="med" />
         <Button text="Button" size="med" alt="secondary"/>
-        <Button text="Button" size="med" alt="accent"/>
-        <Button text="Button" size="med" alt="accent" disabled/>
-        <Button text="Button" size="med" icon="play"/>
+        <Button text="Button" size="med" alt="ghost"/>
+        <Button text="Button" size="med" disabled/>
         <Button text="Button" size="med" icon="play" iconLeft/>
+        <Button text="Button" size="med" arrow/>
     </div>
 
     <div class="buttons">
         <Button text="Button" size="large" />
         <Button text="Button" size="large" alt="secondary"/>
-        <Button text="Button" size="large" alt="accent"/>
-        <Button text="Button" size="large" alt="accent" disabled/>
-        <Button text="Button" size="large" icon="play"/>
+        <Button text="Button" size="large" alt="ghost"/>
+        <Button text="Button" size="large" disabled/>
         <Button text="Button" size="large" icon="play" iconLeft/>
+        <Button text="Button" size="large" arrow/>
     </div>    
 </section>
 ```
@@ -135,7 +145,6 @@ export default {
 
 
 <style lang="postcss">
-
 .buttons {
     margin-bottom: 20px;
     display: flex;
@@ -226,6 +235,17 @@ export default {
     &.c-btn--secondary {
         background-color: #000;
         border-color: #000;
+
+        &:hover,
+        &:active,
+        &:focus {
+            background-color: #fff;            
+            border-color: #000;
+
+            .c-btn__text {
+                color: #000;
+            }
+        }
     }
 
     &.c-btn--accent {
@@ -234,23 +254,31 @@ export default {
     }
 
     &.c-btn--ghost {
-        background-color: #f0f0f0;
-        border-color: #f0f0f0;
+        background-color: #fff;
+        border-color: #ee2e2d;
 
         .c-btn__text {
-            color: #b3b3b3;
+            color: #ee2e2d;
         }
 
         .c-btn__icon {
             use#Mask {
-                fill: #b3b3b3;
+                fill: #ee2e2d;
             }
+        }
+
+        &:hover,
+        &:active,
+        &:focus {
+            background-color: #fff;
+            color: #ee2e2d;
         }
     }
 
     &.c-btn--isDisabled {
         background-color: #f0f0f0;
         border-color: #f0f0f0;
+        cursor: default;
 
         .c-btn__text {
             color: #b3b3b3;
@@ -261,7 +289,23 @@ export default {
                 fill: #b3b3b3;
             }
         }
-    }    
+    }   
+    
+    &.c-btn--arrow {
+
+        &:hover,
+        &:active,
+        &:focus {
+            .c-btn__text {
+
+            }
+
+            .c-btn__arrow {
+                margin-left: 16px;
+                margin-right: -6px;
+            }
+        }
+    }
 }
     .c-btn__inner {
         display: flex;
@@ -271,6 +315,14 @@ export default {
         height: 100%;
     }
 
+        .c-btn__arrow {
+            width: 10px;
+            height: 10px;
+            margin-left: 10px;
+            fill: #fff;
+            transition: all .3s ease;
+        }
+
         .c-btn__text {
             font-family: Gotham;
             font-weight: 700;
@@ -278,6 +330,7 @@ export default {
             font-size: 13px;
             line-height: 16px;
             letter-spacing: 2px;
+            margin-top: 4px;
             color: #fff;
 
             .c-btn--hasIcon & {
--- src/components/Checkbox.vue
+++ src/components/Checkbox.vue
@@ -1,5 +1,10 @@
 <template>
-
+    <div class="c-checkbox"
+        :class="{'is-checked': isChecked}"
+        @click.prevent="toggleCheckbox()"
+    >
+        <span class="c-checkbox__text">{{ text }}</span>  
+    </div>     
 </template>
 
 <script>
@@ -10,30 +15,104 @@ export default {
     components: {
 
     },
+    data: () => {
+        return {
+            isChecked: false
+        }
+    },
 	props:{
-
+        value: {
+            required: true
+        },
+        text: {
+            type: [String, Number],
+            required: true
+        }
 	},
     computed: {
         classObj() {
             return {
-                'c-btn--small': this.size === 'small',
-                'c-btn--med': this.size === 'med',
-                'c-btn--large': this.size === 'large',
+
             }
         },
+    },
+    methods: {
+        toggleCheckbox() {
+            this.isChecked = !this.isChecked
+        }
     }
+
 }
 </script>
 <docs>
 
 ```jsx
 <section>
-  
+    <Checkbox text="checkbox" value="checkbox-1"/>
+    <Checkbox text="checkbox" value="checkbox-2"/>
+    <Checkbox text="checkbox" value="checkbox-3"/>
+    <Checkbox text="checkbox" value="checkbox-4"/>
+    <Checkbox text="checkbox" value="checkbox-5"/>
 </section>
 ```
 </docs>
 
 
 <style lang="postcss">
+.c-checkbox     {
+    position: relative;
+    margin-bottom: 9px;
+    cursor: pointer;
 
+    &::before,
+    &::after {
+        position: absolute;
+        content: "";
+        display: inline-block;
+    }  
+
+    &::before {
+        height: 14px;
+        width: 14px;
+        border: 1px solid #000;
+        left: 0px;
+        top: 3px;
+    }
+
+    &::after {
+        height: 4px;
+        width: 8px;
+        left: 3px;
+        color: white;
+        top: 7px;
+        border-left: 2px solid;
+        border-bottom: 2px solid;
+        -webkit-transform: rotate(-45deg);
+        -ms-transform: rotate(-45deg);
+        transform: rotate(-45deg);
+        content: "";
+        display: none;
+    }  
+
+
+    &.is-checked {
+        &::before {
+            background-color: black;
+        }                        
+        &::after {
+            display: block;
+        }
+    }
+}
+
+    .c-checkbox__text {
+        font-family: Gotham Book;
+        display: block;
+        font-size: 16px;
+        line-height: 1;
+        padding-top: 3px;
+        padding-left: 25px;
+        padding-right: 10px;
+        letter-spacing: 0;
+    }
 </style>
